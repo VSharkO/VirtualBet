b8c9489f215c34b7ae74127d2993529d6568764e
VSharkO <valentin.saric@hotmail.com>
2018-04-03 00:11:42 +0200

    Added calculation funcionality

--- Assets/Scripts/Randomizer.cs
+++ Assets/Scripts/Randomizer.cs
@@ -7,45 +7,169 @@ using UnityEngine.UI;
 
 public class Randomizer : MonoBehaviour {
 
-    public Text klub1,klub2,klub3,klub4,klub5,klub6,klub7,klub8,koef11,koef12,koef1x,koef21,koef22,koef2x,koef31,koef32,koef3x,koef41,koef42,koef4x;
-	// Use this for initialization
-	void Start () {
+    public InputField bet1, bet2, bet3, bet4, betAmount;
+    public Text klub1,klub2,klub3,klub4,klub5,klub6,klub7,klub8,koef11,koef12,koef1x,koef21,koef22,koef2x,koef31,koef32,koef3x,koef41,koef42,koef4x,posibleGain;
+    koef koef1, koef2, koef3, koef4;
+    Club Klub1, Klub2, Klub3, Klub4, Klub5, Klub6, Klub7, Klub8;
+    char[] prognoza;
+    // Use this for initialization
+    void Start () {
         Club[,] parovi = new Club[4, 2];
         parovi = makePairs();
-        klub1.text = parovi[0,0].GetSetIme;
-        klub2.text = parovi[0,1].GetSetIme;
-        klub3.text = parovi[1,0].GetSetIme;
-        klub4.text = parovi[1,1].GetSetIme;
-        klub5.text = parovi[2,0].GetSetIme;
-        klub6.text = parovi[2,1].GetSetIme;
-        klub7.text = parovi[3,0].GetSetIme;
-        klub8.text = parovi[3,1].GetSetIme;
-        koef11.text = MakeKoef(parovi[0, 0], parovi[0, 1]).getSetKoef1.ToString("0.00");
-        koef12.text = MakeKoef(parovi[0, 0], parovi[0, 1]).getSetKoef2.ToString("0.00");
-        koef1x.text = MakeKoef(parovi[0, 0], parovi[0, 1]).getSetKoefX.ToString("0.00");
+        Klub1 = parovi[0,0];
+        Klub2 = parovi[0,1];
+        Klub3 = parovi[1,0];
+        Klub4 = parovi[1,1];
+        Klub5 = parovi[2,0];
+        Klub6 = parovi[2,1];
+        Klub7 = parovi[3,0];
+        Klub8 = parovi[3,1];
 
-        koef21.text = MakeKoef(parovi[1, 0], parovi[1, 1]).getSetKoef1.ToString("0.00");
-        koef22.text = MakeKoef(parovi[1, 0], parovi[1, 1]).getSetKoef2.ToString("0.00");
-        koef2x.text = MakeKoef(parovi[1, 0], parovi[1, 1]).getSetKoefX.ToString("0.00");
+        klub1.text = Klub1.GetSetIme;
+        klub2.text = Klub2.GetSetIme;
+        klub3.text = Klub3.GetSetIme;
+        klub4.text = Klub4.GetSetIme;
+        klub5.text = Klub5.GetSetIme;
+        klub6.text = Klub6.GetSetIme;
+        klub7.text = Klub7.GetSetIme;
+        klub8.text = Klub8.GetSetIme;
 
-        koef31.text = MakeKoef(parovi[2, 0], parovi[2, 1]).getSetKoef1.ToString("0.00");
-        koef32.text = MakeKoef(parovi[2, 0], parovi[2, 1]).getSetKoef2.ToString("0.00");
-        koef3x.text = MakeKoef(parovi[2, 0], parovi[2, 1]).getSetKoefX.ToString("0.00");
+        koef1 = MakeKoef(parovi[0, 0], parovi[0, 1]);
+        koef2 = MakeKoef(parovi[1, 0], parovi[1, 1]);
+        koef3 = MakeKoef(parovi[2, 0], parovi[2, 1]);
+        koef4 = MakeKoef(parovi[3, 0], parovi[3, 1]);
 
-        koef41.text = MakeKoef(parovi[3, 0], parovi[3, 1]).getSetKoef1.ToString("0.00");
-        koef42.text = MakeKoef(parovi[3, 0], parovi[3, 1]).getSetKoef2.ToString("0.00");
-        koef4x.text = MakeKoef(parovi[3, 0], parovi[3, 1]).getSetKoefX.ToString("0.00");
-        Debug.Log(MakeKoef(parovi[0, 0], parovi[0, 1]).getSetKoef1.ToString("0.00"));
+        koef11.text = koef1.getSetKoef1.ToString("0.00");
+        koef12.text = koef1.getSetKoef2.ToString("0.00");
+        koef1x.text = koef1.getSetKoefX.ToString("0.00");
+
+        koef21.text = koef2.getSetKoef1.ToString("0.00");
+        koef22.text = koef2.getSetKoef2.ToString("0.00");
+        koef2x.text = koef2.getSetKoefX.ToString("0.00");
+
+        koef31.text = koef3.getSetKoef1.ToString("0.00");
+        koef32.text = koef3.getSetKoef2.ToString("0.00");
+        koef3x.text = koef3.getSetKoefX.ToString("0.00");
+
+        koef41.text = koef4.getSetKoef1.ToString("0.00");
+        koef42.text = koef4.getSetKoef2.ToString("0.00");
+        koef4x.text = koef4.getSetKoefX.ToString("0.00");
+
+        prognoza = new char[4];
 
     }
-	
+    
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void Update () {
+        
+        
+    }
+    public void ShowPosibleGain() {
+        double posGain;
+        prognoza[0] = bet1.text.ToCharArray()[0];
+        prognoza[1] = bet1.text.ToCharArray()[0];
+        prognoza[2] = bet1.text.ToCharArray()[0];
+        prognoza[3] = bet1.text.ToCharArray()[0];
+        posGain = double.Parse(betAmount.text) * getPosGain();
+        posibleGain.text = posGain.ToString("0.00")+"$";
+        
+        
+    }
+    public double getPosGain() {
+        double gainKoef=1f;
+        if (prognoza[0] == '1')
+        {
+            gainKoef *= koef1.getSetKoef1;
+        }
+        else if (prognoza[0] == '2')
+        {
+            gainKoef *= koef1.getSetKoef2;
+        }
+        else {
+            gainKoef *= koef1.getSetKoefX;
+        }
+        if (prognoza[1] == '1')
+        {
+            gainKoef *= koef2.getSetKoef1;
+        }
+        else if (prognoza[1] == '2')
+        {
+            gainKoef *= koef2.getSetKoef2;
+        }
+        else
+        {
+            gainKoef *= koef2.getSetKoefX;
+        }
+        if (prognoza[2] == '1')
+        {
+            gainKoef *= koef3.getSetKoef1;
+        }
+        else if (prognoza[2] == '2')
+        {
+            gainKoef *= koef3.getSetKoef2;
+        }
+        else
+        {
+            gainKoef *= koef3.getSetKoefX;
+        }
+        if (prognoza[3] == '1')
+        {
+            gainKoef *= koef4.getSetKoef1;
+        }
+        else if (prognoza[3] == '2')
+        {
+            gainKoef *= koef4.getSetKoef2;
+        }
+        else
+        {
+            gainKoef *= koef4.getSetKoefX;
+        }
+        Debug.Log("uloženo: " + double.Parse(betAmount.text));
+        return gainKoef;
+    }
+    public char[] getResults(Club[,] parovi)
+    {
+        double rnd1 = Random.Range(1f, 1.4f);
+        double rnd2 = Random.Range(1f, 1.4f);
+        char[] rezultat = new char[4];
+        double hn1, hn2, hn3, hn4, hn5, hn6, hn7, hn8;
+        hn1 = setHightNum(parovi[0, 0]);
+        hn2 = setHightNum(parovi[0, 1]);
+        hn3 = setHightNum(parovi[1, 0]);
+        hn4 = setHightNum(parovi[1, 1]);
+        hn5 = setHightNum(parovi[2, 0]);
+        hn6 = setHightNum(parovi[2, 1]);
+        hn7 = setHightNum(parovi[3, 0]);
+        hn8 = setHightNum(parovi[3, 1]);
 
+        if (hn1 > hn2) { rezultat[1] = '1'; }
+
+        else if (hn1 < hn2) { rezultat[1] = '2'; }
+
+        else { rezultat[1] = 'x'; }
+
+        if (hn3 > hn4) { rezultat[2] = '1'; }
+
+        else if (hn3 < hn4) { rezultat[2] = '2'; }
+
+        else { rezultat[2] = 'x'; }
+
+        if (hn5 > hn6) { rezultat[3] = '1'; }
+
+        else if (hn5 < hn6) { rezultat[3] = '2'; }
+
+        else { rezultat[3] = 'x'; }
+
+        if (hn7 > hn8) { rezultat[4] = '1'; }
+
+        else if (hn7 < hn8) { rezultat[4] = '2'; }
+
+        else { rezultat[4] = 'x'; }
+
+        return rezultat;
+    }
     //klasa za klub
-    class Club {
+    public class Club {
         private string ime;
         private int strengthHome;
         private int strengthAway;
@@ -214,9 +338,17 @@ public class Randomizer : MonoBehaviour {
         float rnd2 = Random.Range(1f, 2f);
         float rnd3 = Random.Range(1f, 2f);
         koef koeficient = new koef();
-        koeficient.getSetKoef1 = (12F/klub1.GetSetStrHome)+ (12F / klub2.GetSetStrHome)*rnd1;
-        koeficient.getSetKoef2 = 1f+(25F/klub2.GetSetStrAway)*rnd2;
+        koeficient.getSetKoef1 = (40F/klub1.GetSetStrHome)-(12F / klub2.GetSetStrHome)*rnd1;
+        koeficient.getSetKoef2 = 1f+(18F/klub2.GetSetStrAway)*rnd2;
         koeficient.getSetKoefX = (4F + (12F / klub2.GetSetStrAway)+ (12F / klub1.GetSetStrHome))*rnd3;
         return koeficient;
+    }
+
+    
+
+    double setHightNum(Club klub)
+    {
+        double rnd1 = Random.Range(1f, 1.4f);
+        return klub.GetSetStrHome * 0.6f * rnd1 * 0.4f;
     }
 }
